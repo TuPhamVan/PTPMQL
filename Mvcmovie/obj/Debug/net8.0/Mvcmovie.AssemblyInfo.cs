bbbb6eec3a640e58d4608d058f1eced4c1e630ae
TuPhamVan <hipvt203@gmail.com>
2024-09-17 14:26:44 +0700

    CreateProject

--- Mvcmovie/obj/Debug/net8.0/Mvcmovie.AssemblyInfo.cs
+++ Mvcmovie/obj/Debug/net8.0/Mvcmovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218d20d0838a4dbd6ea8e259059f786b76e192af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de569832fc148c20e3e6066cfa2d79bb59dbc492")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
