de569832fc148c20e3e6066cfa2d79bb59dbc492
TuPhamVan <hipvt203@gmail.com>
2024-09-17 14:24:14 +0700

    CreateProject

--- Mvcmovie/Controllers/HelloWorldController.cs
+++ Mvcmovie/Controllers/HelloWorldController.cs
@@ -5,10 +5,9 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     { 
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        } 
+       public IActionResult Index() {
+        return View();
+       }
         // GET: /HelloWorld/Welcome/ 
 
         public string Welcome()
